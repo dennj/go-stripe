44990540272a72f8cd87d829f2b4583dd8345073
Dennj Osele <dennj.osele@gmail.com>
2024-12-24 05:29:57 +0000

    h

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.3
 
 require (
 	github.com/stripe/stripe-go/v81 v81.0.0
+	github.com/tbxark/g4vercel v0.0.4
 	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67
 )
 
--- index.go
+++ index.go
@@ -15,10 +15,47 @@ import (
 	"github.com/stripe/stripe-go/v81/price"
 	"github.com/stripe/stripe-go/v81/webhook"
 	"golang.org/x/exp/slog"
+
+	. "github.com/tbxark/g4vercel"
 )
 
 // Handler is the exported function Vercel uses as the entry point
 func Handler(w http.ResponseWriter, r *http.Request) {
+	server := New()
+
+	server.GET("/", func(context *Context) {
+		context.JSON(200, H{
+			"message": "hello go from vercel !!!!",
+		})
+	})
+	server.GET("/hello", func(context *Context) {
+		name := context.Query("name")
+		if name == "" {
+			context.JSON(400, H{
+				"message": "name not found",
+			})
+		} else {
+			context.JSON(200, H{
+				"data": fmt.Sprintf("Hello %s!", name),
+			})
+		}
+	})
+	server.GET("/user/:id", func(context *Context) {
+		context.JSON(400, H{
+			"data": H{
+				"id": context.Param("id"),
+			},
+		})
+	})
+	server.GET("/long/long/long/path/*test", func(context *Context) {
+		context.JSON(200, H{
+			"data": H{
+				"url": context.Path,
+			},
+		})
+	})
+	server.Handle(w, r)
+
 	switch r.URL.Path {
 	case "/create-checkout-session":
 		createCheckoutSession(w, r)
